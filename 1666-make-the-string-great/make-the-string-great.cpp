47a5ed2d613fa34f874242779f4f4aa7afc0d4a4
Akhilesh <128721569+Akhileshmalik23@users.noreply.github.com>
2025-08-09 02:12:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (66.56%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string stack = "";
+        for (int i = 0; i < s.length(); ++i) {
+            if (!stack.empty() && abs(stack.back() - s[i]) == 32) {
+                stack.pop_back();
+            } else {
+                stack.push_back(s[i]);
+            }
+        }
+        return stack;
+    }
+};
